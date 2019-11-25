70f7b3727283094a045cb89bfdf6dee4ede9ed4e
joelnogueira733@gmail.com <joelnogueira733@gmail.com>
2019-11-25 14:11:09 -0300

    Switch Case

---
+++ Curso C#/Exercícios/EstruturaSwitchCase/EstruturaSwitchCase/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace EstruturaSwitchCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = int.Parse(Console.ReadLine()) ;
+
+            switch (numero)
+            {
+                case 1:
+                    Console.WriteLine("Domimgo");
+                    break;
+                case 2:
+                    Console.WriteLine("Segunda");
+                    break;
+                case 3:
+                    Console.WriteLine("Terça");
+                    break;
+                case 4:
+                    Console.WriteLine("Quarta");
+                    break;
+                case 5:
+                    Console.WriteLine("Quinta");
+                    break;
+                case 6:
+                    Console.WriteLine("Sexta");
+                    break;
+                case 7:
+                    Console.WriteLine("Sábado");
+                    break;
+                default:
+                    Console.WriteLine("Valor inválido");
+                    break;
+            }
+        }
+    }
+}
---
+++ Curso C#/Exercícios/InferenciaDeTipos/InferenciaDeTipos/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace InferenciaDeTipos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = 10;
+            var y = "Maria";
+            var z = 34.32;
+
+            Console.WriteLine(x + ", " +  y + ", " + z);
+        }
+    }
+}
